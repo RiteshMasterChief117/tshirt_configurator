c8b472ffbdfc09886cff635293963beb4b999dea
RiteshMasterChief117 g git config --global user.name RiteshMasterChief117 <riteshdutta117@gmail.com>
2025-10-27 16:01:55 +0530

    Accumulative Shadows and Randomize Light Added

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,18 +1,24 @@
-import { Canvas } from "@react-three/fiber"
+import { Canvas, useFrame } from "@react-three/fiber"
 import './styles.css'
-import { Center, OrbitControls,useGLTF} from "@react-three/drei"
+import { Center, Environment, OrbitControls, useGLTF, AccumulativeShadows, RandomizedLight } from "@react-three/drei"
 import React, { useRef } from 'react'
 
 
-export const App = ({ position = [-1, 0, 2.5], fov = 25 }) => (
-  <Canvas 
-  eventSource={document.getElementById('root')}
-  eventPrefix="client"
-  camera={{ position, fov }}>
-    <ambientLight intensity={0.5}/>
+export const App = ({ position = [0, 0, 2.5], fov = 25 }) => (
+  <Canvas
+    eventSource={document.getElementById('root')}
+    eventPrefix="client"
+    camera={{ position, fov }}>
+    <ambientLight intensity={0.7} />
+    <Environment preset="city" />
+    {/* <CameraRig> */}
+
     <Center>
-       <Shirt />
+      <Shirt />
+      <Backdrop />
     </Center>
+    {/* </CameraRig> */}
+
 
     <OrbitControls />
   </Canvas>
@@ -53,4 +59,49 @@ function Shirt(props) {
     </group>
   )
 }
+
+function Backdrop() {
+  const shadows = useRef()
+
+  return (
+    <AccumulativeShadows
+      ref={shadows}
+      temporal
+      frames={60}
+      alphaTest={0.85}
+      scale={10}
+      rotation={[Math.PI / 2, 0, 0]}
+      position={[0, 0, -0.14]}>
+      <RandomizedLight
+        amount={4}
+        radius={9}
+        intensity={0.55}
+        ambient={0.25}
+        position={[5, 5, -10]}
+      />
+      <RandomizedLight
+        amount={4}
+        radius={5}
+        intensity={0.25}
+        ambient={0.55}
+        position={[-5, 5, -9]}
+      />
+    </AccumulativeShadows>
+  )
+}
+function CameraRig({ children }) {
+  const group = useRef()
+  useFrame((state, delta) => {
+    easing.damp3(state.camera.position, [0, 0, 2], 0.25, delta)
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    )
+  })
+  return <group ref={group}>{children}</group>
+}
+
+
 useGLTF.preload('/TShirtGltfB.glb')
