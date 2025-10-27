a136f17ac8464e05917be614f6ac1b1f02cfa15d
RiteshMasterChief117 g git config --global user.name RiteshMasterChief117 <riteshdutta117@gmail.com>
2025-10-27 15:03:54 +0530

    Orbit Controls

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -1,11 +1,12 @@
 import {Canvas} from "@react-three/fiber"
 import './styles.css'
-import { meshBounds } from "@react-three/drei"
+import { meshBounds,OrbitControls } from "@react-three/drei"
 
 
 export const App=({position=[-1,0,2.5],fov=25}) => (
 <Canvas camera={{position,fov}}>
   <Shirt/>
+  <OrbitControls/>
 </Canvas>
 )
 
